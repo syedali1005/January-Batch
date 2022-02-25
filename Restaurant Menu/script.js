ce0f6617f4579054f27e7a097f208a4331c810d8
Syed Ali Ahmed <67333941+syedali1005@users.noreply.github.com>
2022-02-25 05:00:32 +0500

    Add files via upload

---
+++ Restaurant Menu/script.js
@@ -0,0 +1,40 @@
+
+
+/* Toggle NavBar */
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+}
+
+/* Close Nav when clicking item */
+document.addEventListener("click",function(e){
+    if(e.target.closest(".nav-item")){
+        toggleNav();
+    }
+})
+
+/* sticky header */
+window.addEventListener("scroll",function(){
+    if(this.pageYoffset > 60){
+        document.querySelector(".header").classList.add("sticky");
+    }
+    else{
+        document.querySelector(".header").classList.remove("sticky");
+    }
+});
+
+/* ---------------- Menu tabs --------*/
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click",function(e){
+    if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+        const target = e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const menuSection = document .querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
+    } 
+});
